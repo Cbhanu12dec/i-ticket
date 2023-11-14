e5f8b9ce71e12f64ed1c0cf3d19649e7fe39e599
Cbhanu12dec <cheryalabhanu5@gmail.com>
2023-11-14 17:41:40 -0500

    feat: s3 file upload implmenetation is done

--- client/src/components/tickets/CreateTicketsForm.tsx
+++ client/src/components/tickets/CreateTicketsForm.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   Divider,
+  Flex,
   FormControl,
   FormLabel,
   HStack,
@@ -16,32 +17,72 @@ import {
   Textarea,
   VStack,
 } from "@chakra-ui/react";
+import { UploadProps, message } from "antd";
 import Dragger from "antd/es/upload/Dragger";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { AiOutlineInbox } from "react-icons/ai/index";
+import axios from "axios";
+import { useDropzone } from "react-dropzone";
+
 interface CreateTicketForm {
   showModal: boolean;
   setShowModal: (_open: boolean) => void;
 }
+
+interface TicketForm {
+  title: string;
+  description: string;
+  category: string;
+  priority: string;
+}
 const CreateTicketsForm = (props: CreateTicketForm) => {
   const { setShowModal, showModal } = props;
+  const [file, setFile] = useState();
+  const [ticketPayload, setTicketPayload] = useState<TicketForm>();
+  const { register, handleSubmit } = useForm();
 
-  const { register } = useForm();
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone({});
+
+  const onSubmitClicked = (e: any) => {
+    // e.preventDefault();
+    const formData = new FormData();
+    // formData.append("title", ticketPayload?.title);
+    // formData.append("description", ticketPayload?.description);
+    // formData.append("category", ticketPayload?.category);
+    // formData.append("priority", ticketPayload?.priority);
+    console.log("***** file list:", file);
+    formData.append("files", acceptedFiles[0]);
+    axios
+      .post(`http://localhost:5001/test-upload`, formData, {
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      })
+      .then((res) => {})
+      .catch((err) => console.log("checking error", err));
+  };
   return (
     <Modal isOpen={showModal} onClose={() => setShowModal(false)} size={"4xl"}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Create Ticket Form</ModalHeader>
-        <ModalCloseButton />
-        <Divider />
-        <ModalBody py={"4"}>
-          <form>
+        <form onSubmit={handleSubmit(onSubmitClicked)}>
+          <ModalHeader>Create Ticket Form</ModalHeader>
+          <ModalCloseButton />
+          <Divider />
+          <ModalBody py={"4"}>
             <FormControl>
               <FormLabel>Title</FormLabel>
               <Input
                 type="text"
                 placeholder="Enter Title here..."
                 {...register("title", { required: true })}
+                onChange={(e) =>
+                  setTicketPayload({
+                    ...ticketPayload,
+                    title: e.target.value,
+                  } as any)
+                }
               />
             </FormControl>
             <FormControl mt="3">
@@ -49,6 +90,12 @@ const CreateTicketsForm = (props: CreateTicketForm) => {
               <Textarea
                 placeholder="Enter details here..."
                 {...register("description", { required: true })}
+                onChange={(e) =>
+                  setTicketPayload({
+                    ...ticketPayload,
+                    description: e.target.value,
+                  } as any)
+                }
               />
             </FormControl>
 
@@ -59,11 +106,25 @@ const CreateTicketsForm = (props: CreateTicketForm) => {
                   type="text"
                   placeholder="Enter Title here..."
                   {...register("category", { required: true })}
+                  onChange={(e) =>
+                    setTicketPayload({
+                      ...ticketPayload,
+                      category: e.target.value,
+                    } as any)
+                  }
                 />
               </FormControl>
               <FormControl>
                 <FormLabel>Priority</FormLabel>
-                <Select placeholder="Select option">
+                <Select
+                  placeholder="Select option"
+                  onChange={(e) =>
+                    setTicketPayload({
+                      ...ticketPayload,
+                      priority: e.target.value,
+                    } as any)
+                  }
+                >
                   <option value="low">Low</option>
                   <option value="medium">Medium</option>
                   <option value="high">High</option>
@@ -72,7 +133,7 @@ const CreateTicketsForm = (props: CreateTicketForm) => {
             </HStack>
             <FormControl mt="3">
               <FormLabel>Supporting Documents</FormLabel>
-              <Dragger {...props}>
+              {/* <Dragger {...fileProps}>
                 <VStack gap={1}>
                   <AiOutlineInbox size={"24px"} />
                   <p className="ant-upload-text">
@@ -83,19 +144,42 @@ const CreateTicketsForm = (props: CreateTicketForm) => {
                     from uploading company data or other banned files.
                   </p>
                 </VStack>
-              </Dragger>
+              </Dragger> */}
+              <section className="container">
+                <Flex
+                  alignItems={"center"}
+                  justifyContent={"center"}
+                  {...getRootProps({ className: "dropzone" })}
+                  border={"1px dashed"}
+                  borderColor={"gray.400"}
+                  px="6"
+                  py="4"
+                >
+                  <input {...getInputProps()} />
+                  <p>Drag 'n' drop some files here, or click to select files</p>
+                </Flex>
+              </section>
             </FormControl>
-          </form>
-        </ModalBody>
-        <Divider />
-        <ModalFooter>
-          <Button colorScheme="gray" mr={3} onClick={() => setShowModal(false)}>
-            Close
-          </Button>
-          <Button bg="purple.900" color={"white"} _hover={{ bg: "purple.800" }}>
-            Create Ticket
-          </Button>
-        </ModalFooter>
+          </ModalBody>
+          <Divider />
+          <ModalFooter>
+            <Button
+              colorScheme="gray"
+              mr={3}
+              onClick={() => setShowModal(false)}
+            >
+              Close
+            </Button>
+            <Button
+              bg="purple.900"
+              color={"white"}
+              _hover={{ bg: "purple.800" }}
+              type="submit"
+            >
+              Create Ticket
+            </Button>
+          </ModalFooter>
+        </form>
       </ModalContent>
     </Modal>
   );
--- server/app.js
+++ server/app.js
@@ -1,41 +1,74 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const AWS = require("aws-sdk");
+const fs = require("fs");
+// const fileType = require("file-type");
+const multiparty = require("multiparty");
+const multer = require("multer");
+const cors = require("cors");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+AWS.config.update({
+  accessKeyId: "AKIAYULVJZAKYULF23YV",
+  secretAccessKey: "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",
+});
+
+const s3 = new AWS.S3();
+
+const upload = multer({ storage: multer.memoryStorage() });
+app.post("/test-upload", upload.any("files"), (req, res) => {
+  const file = req.files[0];
+  console.log("filename in req", file.buffer);
+  const params = {
+    Bucket: "i-ticket",
+    Key: file.originalname,
+    Body: file.buffer,
+  };
+
+  s3.upload(params, (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Error uploading file to S3");
+    }
+
+    // File uploaded successfully to S3
+    res.status(200).send("File uploaded to S3");
+  });
+});
+
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
